3cfe6d1e0726994fe809b03425dbf9340b442d38
Dylan Cronk <cronkyyy@gmail.com>
2021-02-19 16:53:10 -0500

    Completed challenges: Add a header to the third section, saying “Amount per person” Add another section showing the total amount for the check – i.e., the original amount plus tip value, without dividing by the number of people. Change the “Number of people” picker to be a text field, making sure to use the correct keyboard type.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State private var checkAmount = ""
+  @State private var numOfPeople = ""
+  @State private var tipPercentage = 2
+  let tipPercentages  = [10,15,20,25,0]
+  
+  var grandTotal: Double {
+    let tipSelection = Double(tipPercentages[tipPercentage])
+    let orderAmount = Double(checkAmount) ?? 0
+    let tipValue = orderAmount * tipSelection / 100
+    
+    return  orderAmount + tipValue
+  }
+  
+  var totalPerPerson: Double {
+    let peopleCount = Double(Int(numOfPeople) ?? 1)
+    
+    return grandTotal / peopleCount
+  }
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Section {
+          TextField("Check amount", text: $checkAmount)
+            .keyboardType(.decimalPad)
+          
+          // Needs additional validation for number input.
+          TextField("Number of people", text: $numOfPeople)
+            .keyboardType(.decimalPad)
+        }
+        
+        Section(header: Text("How much of a tip would you like to leave?")) {
+          Picker("Tip percentage", selection: $tipPercentage) {
+            ForEach(0..<tipPercentages.count) {
+              Text("\(tipPercentages[$0])%")
+            }
+          }
+          .pickerStyle(SegmentedPickerStyle())
+        }
+        
+        Section(header: Text("Amount per person:")) {
+          Text("$\(totalPerPerson, specifier: "%.2f")")
+        }
+        
+        Section(header: Text("Total amount:")) {
+          Text("$\(grandTotal, specifier: "%.2f")")
+        }
+      }
+      .navigationBarTitle("WeSplit")
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
